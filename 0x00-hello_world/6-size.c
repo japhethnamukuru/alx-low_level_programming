879301bb22ed12421d1b47ff443aea09bd0523c4
Namukuru254 <japhethnamukuru@gmail.com>
2022-06-30 14:47:20 -0700

    sizes of basic types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 /**
- *main - entry poiny
+ * main - print sise of basic types
  *
  * Return : Always 0
  */
 int main(void)
 {
 
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a foat: %d byte(s)\n", sizeof(float));
+	char c;
+	int i;
+	long int long_i;
+	long long int long_long_i;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long_i));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long_long_i));
+	printf("Size of a foat: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
