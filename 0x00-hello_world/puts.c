88b5108d81ebbe1dc019c8f8034781fc2dd76d6f
Namukuru254 <japhethnamukuru@gmail.com>
2022-06-30 09:05:09 -0700

    Introducing the I/O: puts

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* 
+ * A c program that prints:
+ *
+ * and ends with a new line
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
