c62a85afafd1506ab137d269ce2741ff2dde52cf
Namukuru254 <japhethnamukuru@gmail.com>
2022-07-06 23:12:00 -0700

    Debugging code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.n"
+
+/**
+ * main - check the positive or negative integer
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
