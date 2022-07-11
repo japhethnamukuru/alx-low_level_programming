8b6af74424e7766fb88ba7bd562e3037c0975d8f
Namukuru254 <japhethnamukuru@gmail.com>
2022-07-11 13:45:48 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - generate passwords
+ *
+ * Return: Integer on success
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, count, s;
+
+	count = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		count += (pass[i] + '0');
+		putchar(pass[i] + '0');
+
+		if ((2772 - count) - '0' < 78)
+		{
+			s = 2772 - count - '0';
+			count += s;
+			putchar(s + '0');
+			break;
+		}
+	}
+	return (0);
+}
+	
