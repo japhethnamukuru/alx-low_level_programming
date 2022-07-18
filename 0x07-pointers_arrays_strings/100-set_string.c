8ef515e362a787e3c9085cac5d00e6466e2b1b28
Namukuru254 <japhethnamukuru@gmail.com>
2022-07-18 14:33:44 -0700

    more pointers, strings and arrays

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - set pointer to char
+ *
+ * @s: pointer to pointer
+ *
+ * @to: pointer to chat
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
