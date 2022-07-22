84fc85ec893f5c2950682ca8ea2e7265896ee7db
Namukuru254 <japhethnamukuru@gmail.com>
2022-07-22 07:13:01 -0700

    Prints number of args to stdout

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - returns numbers of arguments passed to the program
+ *
+ * @argc: argument count
+ *
+ * @argv: 1D string array of arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%i\n", argc);
+	return (0);
+}
